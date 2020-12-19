03c8f717ef691af8d8248e370653d0639bb3f855
darlan <darlan@darlan-S430-G-BE41P1>
2020-12-19 18:11:11 -0300

    Primeiro commit classe java

---
+++ src/main/java/br/edu/ufop/Interval.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ufop;
+
+/**
+ *
+ * @author Darlan
+ * @author Jonata
+ */
+public class Interval {
+    private double inf;
+    private double sup;
+}
