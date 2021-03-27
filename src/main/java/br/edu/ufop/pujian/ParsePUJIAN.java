547dde2e30ff3b37dcdaa4f760a74432d9ee6b7a
darlan <darlan@ufop.edu.br>
2021-03-27 14:39:28 -0300

    Start to implements read commands from file.

--- src/main/java/br/edu/ufop/pujian/ParsePUJIAN.java
+++ src/main/java/br/edu/ufop/pujian/ParsePUJIAN.java
@@ -25,6 +25,11 @@ import anasy.whitespace.Whitespace;
 import java.util.ArrayList;
 import java.util.List;
 
+import java.io.File;  // Import the File class
+import java.io.FileNotFoundException;  // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+
 /**
  * A parser to operations with {@link Interval}
  * 
@@ -537,4 +542,20 @@ public class ParsePUJIAN extends InteractiveParser<Node>{
         }
     }
     
+    public class ComandsFromFile{
+        public void ReadFromFile(String filename){
+            try{
+                File myObj = new File(filename);
+                Scanner myReader = new Scanner(myObj);
+                while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+                myReader.close();
+            } catch (FileNotFoundException e) {
+                System.out.println("An error occurred.");
+                e.printStackTrace();
+            }
+        }
+    }
 }
